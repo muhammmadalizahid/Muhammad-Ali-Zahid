b077612123968aaec6229b581887729e071b691f
Muhammad Ali Zahid <muhammadalizahid0621@gmail.com>
2024-09-12 08:52:48 -0700

    Add files via upload

---
+++ textpacman.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"PPPPPPPPP AAAAAAAA CCCCCCCCCCC MMM     MMM AAAAAAAA NNNN      NN"<<endl;
+cout<<"PP     PP AA    AA CC          MM M   M MM AA    AA NN NN     NN"<<endl;
+cout<<"PP     PP AA    AA CC          MM MM MM MM AA    AA NN  NN    NN"<<endl;
+cout<<"PPPPPPPPP AAAAAAAA CC          MM   M   MM AAAAAAAA NN   NN   NN"<<endl;
+cout<<"PP        AA    AA CC          MM       MM AA    AA NN    NN  NN"<<endl;
+cout<<"PP        AA    AA CC          MM       MM AA    AA NN     NN NN"<<endl;
+cout<<"PP        AA    AA CCCCCCCCCCC MM       MM AA    AA NN      NNNN"<<endl;
+}
+
