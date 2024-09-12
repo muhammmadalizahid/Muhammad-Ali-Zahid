ffe7e75540820d7221e3f184661f6ff729dee242
muhammadalizahid <muhammadalizahid0621@gmail.com>
2024-09-12 06:21:31 -0700

    pfhshapesassignment

---
+++ hexagonassignment.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"          ________________ "<<endl;
+cout<<"         /                \\"<<endl; 
+cout<<"        /                  \\"<<endl;     
+cout<<"       /                    \\"<<endl;
+cout<<"      /                      \\"<<endl;
+cout<<"      \\                      /"<<endl;
+cout<<"       \\                    /"<<endl;
+cout<<"        \\                  /"<<endl;
+cout<<"         \\________________/";
+}
+
+
+
