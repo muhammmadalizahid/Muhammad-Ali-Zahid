b3330f2b2bbbc4f179ca0fb1ab85dcacd60280a4
Muhammad Ali Zahid <muhammadalizahid0621@gmail.com>
2024-09-12 08:47:32 -0700

    Add files via upload

---
+++ nametextassignment.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<" AAAAAAAA"<<endl;
+cout<<" AA    AA"<<endl;
+cout<<" AA    AA"<<endl;
+cout<<" AAAAAAAA"<<endl;
+cout<<" AA    AA"<<endl;
+cout<<" AA    AA"<<endl;
+cout<<" AA    AA"<<endl<<endl;
+cout<<" LL"<<endl;
+cout<<" LL"<<endl;
+cout<<" LL"<<endl;
+cout<<" LL"<<endl;
+cout<<" LL"<<endl;
+cout<<" LLLLLLLLL"<<endl<<endl;
+cout<<" IIIIIIIIII"<<endl;
+cout<<"     II"<<endl;
+cout<<"     II"<<endl;
+cout<<"     II"<<endl;
+cout<<"     II"<<endl;
+cout<<"     II"<<endl;
+cout<<" IIIIIIIIII";
+}
+
